1785f722ffc2f9ccc22a6875cc12b077d3b98062
shanj <605284854@qq.com>
2020-06-30 21:21:26 +0800

    /web-project

--- AJAX_1/server.js
+++ AJAX_1/server.js
@@ -27,56 +27,60 @@ var server = http.createServer(function (request, response) {
   // console.log('request.headers')
   // console.log(request.headers) //读取请求头
 
-  if (path === "/AJAX_1/public/index.html") {
+  if (path === "/web-project/AJAX_1/public/index.html") {
     response.statusCode = 200; //设置响应状态码
     response.setHeader("Content-Type", "text/html;charset=utf-8"); //设置响应头
-    let string = fs.readFileSync("AJAX_1/public/index.html").toString(); //得到的内容可能不是string类型，需要toString()
-    const page1 = fs.readFileSync("AJAX_1/db/page1.json").toString(); //得到的内容可能不是string类型，需要toString()
+    let string = fs
+      .readFileSync("/web-project/AJAX_1/public/index.html")
+      .toString(); //得到的内容可能不是string类型，需要toString()
+    const page1 = fs
+      .readFileSync("/web-project/AJAX_1/db/page1.json")
+      .toString(); //得到的内容可能不是string类型，需要toString()
     const array = JSON.parse(page1);
     const result = array.map((item) => `<li>${item.id}</li>`).join("");
     string = string.replace("{{page1}}", `<ul id='xxx'>${result}</ul>`); //把index.html中读取的string字符串中的{{page1}}占位符内容的地方替换为数据库中拿到的page1字符串
     response.write(string); //response.write() 写入相应内容  括号内的字符串用反引号括起来，反引号中间的内容可换行编写
     //设置响应体
     response.end(); // response.end() 调用end，则响应发送给浏览器
-  } else if (path === "/AJAX_1/public/main.js") {
+  } else if (path === "/web-project/AJAX_1/public/main.js") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/javascript;charset=utf-8");
-    response.write(fs.readFileSync("/AJAX_1/public/main.js"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/public/main.js"));
     response.end();
-  } else if (path === "/AJAX_1/public/2.js") {
+  } else if (path === "/web-project/AJAX_1/public/2.js") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/javascript;charset=utf-8");
-    response.write(fs.readFileSync("AJAX_1/public/2.js"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/public/2.js"));
     response.end();
-  } else if (path === "/AJAX_1/public/style.css") {
+  } else if (path === "/web-project/AJAX_1/public/style.css") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/css;charset=utf-8");
-    response.write(fs.readFileSync("AJAX_1/public/style.css"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/public/style.css"));
     response.end();
-  } else if (path === "/AJAX_1/public/3.html") {
+  } else if (path === "/web-project/AJAX_1/public/3.html") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/html;charset=utf-8");
-    response.write(fs.readFileSync("AJAX_1/public/3.html"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/public/3.html"));
     response.end();
-  } else if (path === "/AJAX_1/public/4.xml") {
+  } else if (path === "/web-project/AJAX_1/public/4.xml") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "text/xml;charset=utf-8");
-    response.write(fs.readFileSync("AJAX_1/public/4.xml"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/public/4.xml"));
     response.end();
-  } else if (path === "/AJAX_1/public/5.json") {
+  } else if (path === "/web-project/AJAX_1/public/5.json") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json;charset=utf-8"); //这里可以写为text/json或者pplication/json
-    response.write(fs.readFileSync("AJAX_1/public/5.json"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/public/5.json"));
     response.end();
-  } else if (path === "/AJAX_1/db/page2.json") {
+  } else if (path === "/web-project/AJAX_1/db/page2.json") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json;charset=utf-8"); //这里可以写为text/json或者pplication/json
-    response.write(fs.readFileSync("AJAX_1/db/page2.json"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/db/page2.json"));
     response.end();
-  } else if (path === "/AJAX_1/db/page3.json") {
+  } else if (path === "/web-project/AJAX_1/db/page3.json") {
     response.statusCode = 200;
     response.setHeader("Content-Type", "application/json;charset=utf-8"); //这里可以写为text/json或者pplication/json
-    response.write(fs.readFileSync("AJAX_1/db/page3.json"));
+    response.write(fs.readFileSync("/web-project/AJAX_1/db/page3.json"));
     response.end();
   } else {
     response.statusCode = 404;
