c565a885b127b4e513b1b8c730bb8cdc30a3a76a
shanj0508 <605284854@qq.com>
2021-01-12 17:16:39 +0800

    svg-sprite-loader引入svg

--- account-vue/src/components/Nav.vue
+++ account-vue/src/components/Nav.vue
@@ -1,7 +1,13 @@
 <template>
     <!-- 底部导航栏 -->
     <div class="nav">
-        <router-link to="/labels">标签
+        <router-link to="/labels">
+        <svg>
+            <use xlink:href="#labels" />   
+            <!-- #labels 是svg的id -->
+        </svg>
+        标签
+
         </router-link>|
         <router-link to="/money">记账
         </router-link>|
@@ -10,6 +16,9 @@
 </template>
 
 <script lang="ts">
+import x from "@/assets/icons/labels.svg";
+console.log(x)
+
     export default {
         name: "Nav"
     };
