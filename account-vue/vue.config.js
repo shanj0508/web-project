925c3f5e841c60ae30c17d90bdc2732489ab0b1d
shanj <605284854@qq.com>
2021-01-13 23:08:44 +0800

    fix bug

--- account-vue/vue.config.js
+++ account-vue/vue.config.js
@@ -10,6 +10,9 @@ module.exports = {
             .test(/\.svg$/)    //以.svg结尾的
             .include.add(dir).end()  //只包含icons目录 才匹配上面的规则
             .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()  //extract:false 表示不需要解析出文件
+            //svgo-loader 批量删除svg文件的fill属性
+            .use('svgo-loader').loader('svgo-loader')
+            .tap(options=>({...options,plugin: [{removeAttrs:{attrs:'fill'}}]})).end()
         //配置插件
         config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSpride: true}])
         config.module.rule('svg').exclude.add(dir)  //其他svg loader排除icons目录
