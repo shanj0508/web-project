1919c887e3f8c34e7b062dc63ee8f3cf6553f557
shanj <605284854@qq.com>
2021-01-13 15:57:45 +0800

    添加nav样式

--- account-vue/src/components/Layout.vue
+++ account-vue/src/components/Layout.vue
@@ -16,13 +16,13 @@
 
 <style lang="scss" scoped>
 .nav-wrapper{
-    border:1px solid green;
+    /*border:1px solid green;*/
     display: flex;
     flex-direction: column;
     height: 100vh;
 }
 .content{
-    border:1px solid blue;
+    /*border:1px solid blue;*/
     flex-grow: 1;   //让content区域尽量占满全屏
     overflow:auto;    //自动出滚动条
 }
