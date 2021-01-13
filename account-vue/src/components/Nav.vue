7876ddbf0c3e8738546552c629178cbcd8cb681d
shanj <605284854@qq.com>
2021-01-13 10:40:09 +0800

    创建 <Icon />

--- account-vue/src/components/Nav.vue
+++ account-vue/src/components/Nav.vue
@@ -2,40 +2,23 @@
     <!-- 底部导航栏 -->
     <div class="nav">
         <router-link to="/labels">
-            <svg>
-                <use xlink:href="#labels"/>
-                <!-- #labels 是svg的id -->
-            </svg>
+         <Icon name="labels"/>
             标签
         </router-link>
         |
         <router-link to="/money">
-            <svg>
-                <use xlink:href="#money"/>
-
-            </svg>
+            <Icon name="money"/>
             记账
         </router-link>
         |
         <router-link to="/statistics">
-            <svg>
-                <use xlink:href="#statistics"/>
-
-            </svg>
+            <Icon name="statistics"/>
             统计
         </router-link>
     </div>
 </template>
 
 <script lang="ts">
-    import x1 from "@/assets/icons/labels.svg";
-    import x2 from "@/assets/icons/money.svg";
-    import x3 from "@/assets/icons/statistics.svg";
-
-    console.log(x1);
-    console.log(x2);
-    console.log(x3);
-
     export default {
         name: "Nav"
     };
