e74ebb0130cdf905f17cbf172784b5baab7261bf
shanj <605284854@qq.com>
2021-01-15 22:15:12 +0800

    添加Money-tags局部样式和变量

--- account-vue/src/App.vue
+++ account-vue/src/App.vue
@@ -13,6 +13,7 @@ body{
     -moz-osx-font-smoothing: grayscale;
     line-height: 1.5;
     font-family: $font-hei;
+    font-size: 16px;
     color: #333;
 }
 </style>
--- account-vue/src/views/Money.vue
+++ account-vue/src/views/Money.vue
@@ -2,12 +2,17 @@
 
     <Layout>
         <div class="tags">
+
             <ul class="current">
                 <li>衣</li>
                 <li>食</li>
                 <li>住</li>
                 <li>行</li>
             </ul>
+            <div class="new">
+                <button>新增标签</button>
+            </div>
+
         </div>
         <div>
             <label class="notes">
@@ -50,5 +55,34 @@
     };
 </script>
 <style lang="scss" scoped>
+    .tags{
+        font-size: 14px;
+        padding: 16px;
+        > .current{
+            display: flex;
+
+            > li{
+                background: #d9d9d9;
+                $h: 24px;
+                border-radius: $h/2;
+                padding: 0 16px;
+                margin-right: 12px;
+                line-height:$h;
+
+            }
+
+        }
+        > .new{
+            padding-top: 16px;
+            button{
+                background: transparent;
+                border: none;
+                color:#999;
+                border-bottom: 1px solid;
+                padding: 0 4px;
+
+            }
+        }
+    }
 
 </style>
