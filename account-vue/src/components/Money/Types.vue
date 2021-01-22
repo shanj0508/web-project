75e907cb0a9af526d5b13105d1524d47b2d27c30
shanj <605284854@qq.com>
2021-01-22 17:45:35 +0800

    add tslib modules

--- account-vue/src/components/Money/Types.vue
+++ account-vue/src/components/Money/Types.vue
@@ -31,16 +31,11 @@
 <!--</script>-->
 <script lang="ts">
     import Vue from 'vue';
-    import Component from 'vue-property-decorator';
+    import {Component} from 'vue-property-decorator';
 
-    @Component({
-        props: {
-            propMessage: String
-        }
-    })
+    @Component
     export default class Types extends Vue {
         type = "-";   //'-'表示支出； '+'表示收入；
-        helloMsg ='Hello, '+this.propMessage;
         selectType(type: string) {  //type只能是'-' 和'+'中的一个
             if (type !== "-" && type !== "+") {
                 throw new Error("type is unknown");
