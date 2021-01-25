e37b091f9b5d0513535f1bd70773d3c006720c82
shanj0508 <605284854@qq.com>
2021-01-25 18:53:20 +0800

    添加created version字段

--- account-vue/src/components/Money/NumberPad.vue
+++ account-vue/src/components/Money/NumberPad.vue
@@ -70,6 +70,7 @@
         ok() {
             this.$emit('update:value',this.output)
             this.$emit('submit',this.output)
+            this.output= '0'
 
         }
     }
--- account-vue/src/views/Money.vue
+++ account-vue/src/views/Money.vue
@@ -4,7 +4,7 @@
         <Types :value.sync="record.type"/>
         <Notes @update:value="onUpdateNotes"/>
         <Tags :dataSource.sync="tags" @update:value="onUpdateTags"/>
-        {{record}}
+        {{recordList}}
     </Layout>
 
 </template>
@@ -17,12 +17,27 @@
     import Types from '@/components/Money/Types.vue';
     import Tags from '@/components/Money/Tags.vue';
     import Notes from '@/components/Money/Notes.vue';
+    //数据库版本
+    const version=window.localStorage.getItem('version')||'0'
+    const recordList:Record[]=JSON.parse(window.localStorage.getItem("recordList")||'[]');
+    //数据库升级，数据迁移
+    if(version==='0.0.1'){
+        recordList.forEach(record=>{
+            record.createdAt=new Date(0)
+        })
+        //保存数据
+        window.localStorage.setItem('recordList',JSON.stringify(recordList));
+    }
+    //更新版本号
+    window.localStorage.setItem('version','0.0.2')
+
     //ts类型声明
     type Record = {
         tags: string[]
         notes: string
         type: string
-        amount: number
+        amount: number    // 数据类型object | string
+        createdAt?:Date    // 类 / 构造函数   ?表示可以createdAt为undefined不存在
     }
 
     @Component(
@@ -32,7 +47,7 @@
     )
     export default class Money extends Vue {
         tags=['衣','食','住','行'];
-        recordList:Record[]=[];
+        recordList:Record[]=recordList;
         record: Record={
             tags:[],
             notes:'',
@@ -52,8 +67,9 @@
         //     this.record.amount=parseFloat(value);
         // }
         saveRecord(){
-            //deepClone
-            const record2=JSON.parse(JSON.stringify(this.record));
+            //深拷贝
+            const record2:Record = JSON.parse(JSON.stringify(this.record));
+            record2.createdAt=new Date()
             this.recordList.push(record2);
             console.log(this.recordList);
             
