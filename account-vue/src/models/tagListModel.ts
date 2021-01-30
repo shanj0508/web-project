a98d290e40586b350536bc6520cafb9e525fe85b
shanj <605284854@qq.com>
2021-01-30 12:38:40 +0800

    实现编辑和删除标签功能

--- account-vue/src/models/tagListModel.ts
+++ account-vue/src/models/tagListModel.ts
@@ -9,6 +9,8 @@ type TagListModel = {
     fetch: () => Tag[]  //输入的参数类型=>输出的结果类型
     create: (name: string) => 'success' | 'duplicated'   //联合类型
     save: () => void   //void表示为空
+    update: (id: string, name: string) => 'success' | 'not found' | 'duplicated'
+    remove: (id: string) => boolean
 }
 const tagListModel: TagListModel = {
     data: [],
@@ -25,9 +27,38 @@ const tagListModel: TagListModel = {
         if (names.indexOf(name) >= 0) {
             return 'duplicated';    //表示文件名重复
         }
-        this.data.push({id:name,name:name});
+        this.data.push({id: name, name: name});
         this.save();
         return 'success';    //表示成功
+
+    },
+    update(id, name) {
+        const idList = this.data.map(item => item.id);
+        if (idList.indexOf(id) >= 0) {
+            const names = this.data.map(item => item.name);
+            if (names.indexOf(name) >= 0) {
+                return 'duplicated';
+            } else {
+                const tag = this.data.filter(item => item.id === id)[0];
+                tag.name = name;
+                this.save();
+                return 'success';
+            }
+        } else {
+            return 'not found';
+        }
+    },
+    remove(id: string) {
+        let index = -1;
+        for (let i = 0; i < this.data.length; i++) {
+            if (this.data[i].id === id) {
+                index = i;
+                break;
+            }
+        }
+        this.data.splice(index, 1);
+        this.save()
+        return true;
 
     }
 };
