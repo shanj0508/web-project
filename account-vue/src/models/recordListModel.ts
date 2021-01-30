23e5bcaa38460f1caa1e68b6d49f95bf66d20d95
shanj <605284854@qq.com>
2021-01-30 16:28:02 +0800

    重新封装recordListModel.ts

--- account-vue/src/models/recordListModel.ts
+++ account-vue/src/models/recordListModel.ts
@@ -1,13 +1,15 @@
 const localStorageKeyName = 'recordList';
 const recordListModel = {
+    data: [] as RecordItem[],
     clone(data: RecordItem[] | RecordItem) {
         return JSON.parse(JSON.stringify(data));
     },
     fetch() {
-        return JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];   //强制声明
+        this.data = JSON.parse(window.localStorage.getItem(localStorageKeyName) || '[]') as RecordItem[];   //强制声明
+        return this.data;
     },
-    save(data: RecordItem[]) {
-        window.localStorage.setItem(localStorageKeyName, JSON.stringify(data));
+    save() {
+        window.localStorage.setItem(localStorageKeyName, JSON.stringify(this.data));
     }
 };
 
