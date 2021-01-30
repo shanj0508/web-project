824d47f445fdbe8f96f6fa6efb9746fd6c4116a8
shanj <605284854@qq.com>
2021-01-30 12:09:08 +0800

    编辑页面展示tag的name

--- account-vue/src/views/EditLabel.vue
+++ account-vue/src/views/EditLabel.vue
@@ -8,7 +8,7 @@
             <span class="rightIcon"></span>
         </div>
         <div class="form-wrapper">
-            <FormItem field-name="标签名"
+            <FormItem :value="tag.name" field-name="标签名"
                       placeholder="请输入标签名"/>
         </div>
         <div class="button-wrapper">
@@ -28,13 +28,16 @@
         components: {Button, FormItem}
     })
     export default class EditLabel extends Vue {
+        tag?: { id: string, name: string } = undefined;
+
         created() {
             const id = this.$route.params.id;
             tagListModel.fetch();
             const tags = tagListModel.data;
             const tag = tags.filter(t => t.id === id)[0];
             if (tag) {
-                console.log(tag);
+                this.tag = tag;
+
             } else {
                 this.$router.replace("/404");
             }
@@ -73,7 +76,8 @@
         background: white;
         margin-top: 8px;
     }
-    .button-wrapper{
+
+    .button-wrapper {
         text-align: center;
         padding: 16px;
         margin-top: 44-16px;
