475bbd8260becd835170c506baff4b41283ff409
shanj <605284854@qq.com>
2021-01-31 17:06:59 +0800

    全部改写Vuex进行数据管理

--- account-vue/src/components/Money/FormItem.vue
+++ account-vue/src/components/Money/FormItem.vue
@@ -5,49 +5,40 @@
             <input type="text"
                    :value="value"
                    @input="onValueChanged($event.target.value)"
-                   :placeholder="placeholder">
+                   :placeholder="this.placeholder">
         </label>
     </div>
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-    import {Component, Prop} from "vue-property-decorator";
-
+    import Vue from 'vue';
+    import {Component, Prop} from 'vue-property-decorator';
     @Component
     export default class FormItem extends Vue {
-       @Prop({default:''}) readonly value! :string;
-        // 监听value，发生变化时触发
-        //{required:true} 表示必填
+        @Prop({default: ''}) readonly value!: string;
         @Prop({required: true}) fieldName!: string;
-        @Prop() placeholder?: string;   //?表示有可能不存在
-
-
-        onValueChanged(value: String) {
-            this.$emit("update:value", value);
+        @Prop() placeholder?: string;
+        onValueChanged(value: string) {
+            this.$emit('update:value', value);
         }
     }
 </script>
 
 <style lang="scss" scoped>
     .formItem {
-        display: flex;
         font-size: 14px;
-        /*background: #E5E5E5;*/
         padding-left: 16px;
+        display: flex;
         align-items: center;
-
         .name {
             padding-right: 16px;
         }
-
         input {
             height: 40px;
             flex-grow: 1;
-            background: transparent; /*背景色透明*/
+            background: transparent;
             border: none;
             padding-right: 16px;
         }
-
     }
 </style>
