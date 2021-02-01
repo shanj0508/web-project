4fb8fc6ff3291c228c76f98706fc2e9194a2087c
shanj <605284854@qq.com>
2021-02-01 23:44:21 +0800

    部署网站到account-website

--- account-vue/vue.config.js
+++ account-vue/vue.config.js
@@ -1,6 +1,9 @@
 const path = require('path');  //引入path模块
 
 module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+        ? '/account-website/'
+        : '/',
     lintOnSave: false,
     chainWebpack: config => {
         const dir = path.resolve(__dirname, 'src/assets/icons')  //__dirname表示当前目录
@@ -10,9 +13,9 @@ module.exports = {
             .test(/\.svg$/)    //以.svg结尾的
             .include.add(dir).end()  //只包含icons目录 才匹配上面的规则
             .use('svg-sprite-loader').loader('svg-sprite-loader').options({extract: false}).end()  //extract:false 表示不需要解析出文件
-            //svgo-loader 批量删除svg文件的fill属性
+        //svgo-loader 批量删除svg文件的fill属性
             .use('svgo-loader').loader('svgo-loader')
-            .tap(options=>({...options,plugins: [{removeAttrs:{attrs:'fill'}}]})).end()
+            .tap(options => ({...options, plugins: [{removeAttrs: {attrs: 'fill'}}]})).end()
         //配置插件
         config.plugin('svg-sprite').use(require('svg-sprite-loader/plugin'), [{plainSprite: true}])
         config.module.rule('svg').exclude.add(dir)  //其他svg loader排除icons目录
