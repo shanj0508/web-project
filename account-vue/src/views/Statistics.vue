888b2b3b9070d6cedba5fd88d3ca18baf31e6427
shanj <605284854@qq.com>
2021-02-19 15:07:13 +0800

    使用Echarts组件

--- account-vue/src/views/Statistics.vue
+++ account-vue/src/views/Statistics.vue
@@ -1,6 +1,7 @@
 <template>
     <Layout>
         <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
+        <ECharts :options="x"/>
         <ol v-if="groupedList.length>0">
             <li v-for="(group, index) in groupedList" :key="index">
                 <h3 class="title">{{beautify(group.title)}} <span>￥{{group.total}}</span></h3>
@@ -24,9 +25,14 @@
     import recordTypeList from "@/constants/recordTypeList";
     import dayjs from "dayjs";
     import clone from "@/lib/clone";
+    // import ECharts from 'vue-echarts'
+    const ECharts:any = require("vue-echarts").default;
+    console.log(ECharts);
+    import 'echarts/lib/chart/line'
+    import 'echarts/lib/component/polar'
 
     @Component({
-        components: {Tabs},
+        components: {Tabs,ECharts},
     })
     export default class Statistics extends Vue {
         tagString(tags: Tag[]) {
@@ -48,7 +54,50 @@
                 return day.format("YYYY年M月D日");
             }
         }
+        get x(){
+            let data = []
 
+            for (let i = 0; i <= 360; i++) {
+                let t = i / 180 * Math.PI
+                let r = Math.sin(2 * t) * Math.cos(2 * t)
+                data.push([r, i])
+            }
+            return{
+                title: {
+                    text: '极坐标双数值轴'
+                },
+                legend: {
+                    data: ['line']
+                },
+                polar: {
+                    center: ['50%', '54%']
+                },
+                tooltip: {
+                    trigger: 'axis',
+                    axisPointer: {
+                        type: 'cross'
+                    }
+                },
+                angleAxis: {
+                    type: 'value',
+                    startAngle: 0
+                },
+                radiusAxis: {
+                    min: 0
+                },
+                series: [
+                    {
+                        coordinateSystem: 'polar',
+                        name: 'line',
+                        type: 'line',
+                        showSymbol: false,
+                        data: data
+                    }
+                ],
+                animationDuration: 2000
+
+            }
+        }
         get recordList() {
             return (this.$store.state as RootState).recordList;
         }
@@ -93,6 +142,10 @@
 </script>
 
 <style scoped lang="scss">
+    .echarts {
+        max-width: 100%;
+        height: 400px;
+    }
     .noResult {
         padding: 16px;
         text-align: center;
@@ -126,7 +179,7 @@
 
     .title {
         @extend %item;
-        background:whitesmoke;
+        background: whitesmoke;
     }
 
     .record {
