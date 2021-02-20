6915410e07157701da9051d6798d1e3e57d83f02
shanj <605284854@qq.com>
2021-02-20 20:26:11 +0800

    React demo:点击按钮+1

--- react-demo/src/App.js
+++ react-demo/src/App.js
@@ -1,10 +1,51 @@
 import React from 'react'
+// import "style.css"
 
-const App = () => {
-    return (
-        //这个div相当于 React.createElement()
-        <div>App组件</div>
-    )
+
+export default class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            x: 1
+        }
+    }
+
+    onClick = () => {
+        this.setState((state) => {
+            return {x: state.x + 1}
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                App <button onClick={this.onClick}>+1</button>
+                <B n={this.state.x}/>
+            </div>
+        );
+    }
 }
 
-export default App
+class B extends React.Component {
+    componentWillReceiveProps(newProps) {
+        console.log('旧的 props 为')
+        console.log(this.props)
+        console.log('新的 props 为')
+        console.log(newProps)
+        // 注意 console 的延迟计算 bug
+    }
+
+    render() {
+        return <div>B {this.props.n}</div>
+    }
+}
+
+
+
+
+
+
+
+
+
+
